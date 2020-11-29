61c1b7ed99846432647ab76236665f89052f99fd
Mehmet <mehmetpekdemir06@hotmail.com>
2020-11-29 16:33:08 +0300

    Clean-Architecture project created.

---
+++ backend/src/main/java/com/mehmetpekdemir/cleanarchitecture/CleanarchitectureApplication.java
@@ -0,0 +1,18 @@
+package com.mehmetpekdemir.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@SpringBootApplication
+public class CleanarchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchitectureApplication.class, args);
+	}
+
+}
